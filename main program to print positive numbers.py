7caf14da37c39d14c39a5e6bdfc54372c1e4ad12
shashushaikshavali786 <71346356+shashushaikshavali786@users.noreply.github.com>
2020-09-17 00:03:39 +0530

    output positive number
    giving output positive numbers from a list of positive&negative numbers

---
+++ main program to print positive numbers.py
@@ -0,0 +1,3 @@
+list1 = [12,-7,5,64,-14]
+output = list(filter(lambda x: x > 0, list1))
+print("output: ",output)
---
+++ main program2 of positivenumber.py
@@ -0,0 +1,3 @@
+list1 = [12,14,-95,3]
+output = list(filter(lambda x: x > 0, list1))
+print("output: ",output)
